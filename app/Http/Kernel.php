6c62256ca2beed468d1d92328d822f078394b3f6
haqqer <ilhamserbaguna@gmail.com>
2019-11-14 08:34:45 +0700

    change cors middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,7 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'jwt.verify' => \App\Http\Middleware\JwtMiddleware::class,
-        'cors' => \Barryvdh\Cors\HandleCors::class
+        'cors' => \App\Http\Middleware\CorsMiddleware::class
     ];
 
     /**
