609519aeed6608df9cb96cf56c02065e2461de35
haqqer <ilhamserbaguna@gmail.com>
2019-11-14 08:37:00 +0700

    update Middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,8 +16,8 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin:  *')
-            ->header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE')
-            ->header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
     }
 }
--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -34,9 +34,6 @@ class JwtMiddleware
                 return response()->json(['message' => 'Authorization Token Not Found'], 403);
             }
         }
-        return $next($request)
-            ->header('Access-Control-Allow-Origin:  *')
-            ->header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE')
-            ->header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');        
+        return $next($request);    
     }
 }
