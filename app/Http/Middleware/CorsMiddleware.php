4dfddc4aadad77ff1b1fbd3371b23b660c6b2dc5
haqqer <ilhamserbaguna@gmail.com>
2019-11-16 02:05:58 +0000

    fix cors with header on cors middleware

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,9 +15,9 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization, X-Requested-With, Application, undefined');
+        return $next($request);
     }
 }
